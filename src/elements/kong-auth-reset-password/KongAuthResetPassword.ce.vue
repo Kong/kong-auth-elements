66b5372bb7b9979e23c84423600ad0aa6169abc8
Adam DeHaven <adam.dehaven@konghq.com>
2021-12-09 18:55:53 -0500

    fix(build): remove empty props
    Remove empty props to fix build.

--- src/elements/kong-auth-reset-password/KongAuthResetPassword.ce.vue
+++ src/elements/kong-auth-reset-password/KongAuthResetPassword.ce.vue
@@ -12,18 +12,11 @@ import ResetPassword from '@/components/ResetPassword.vue'
 export default defineComponent({
   name: 'KongAuthResetPassword',
 
-  // Props are defined here for use on the custom element tag
-  props: {},
-
   emits: ['reset-password-success'],
 
   components: {
     BaseCustomElement,
     ResetPassword,
-  },
-
-  setup(props) {
-    // Provide custom element props to child components
   },
 })
 </script>
