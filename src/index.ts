b49a67cc5b530bc9a6188487f468bd45fc07427b
Adam DeHaven <adam.dehaven@konghq.com>
2021-12-13 18:05:28 -0500

    fix(api): export kong auth api
    Export KongAuthApi as the default export for consuming
    applications to utilize the kauth API.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { registerKongAuthLogin } from '@/elements/kong-auth-login'
 import { registerKongAuthForgotPassword } from '@/elements/kong-auth-forgot-password'
 import { registerKongAuthResetPassword } from '@/elements/kong-auth-reset-password'
 import { registerKongAuthRegister } from '@/elements/kong-auth-register'
+import KongAuthApi from '@/services/Api'
 
 registerKongAuthLogin()
 registerKongAuthForgotPassword()
 registerKongAuthResetPassword()
 registerKongAuthRegister()
+
+export default KongAuthApi
