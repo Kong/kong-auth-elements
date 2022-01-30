6e0d63924583a884c758fd20369434482ba843f7
Adam DeHaven <adam.dehaven@konghq.com>
2022-01-30 17:52:17 -0500

    chore(kebabize): fixing function

--- src/utils/kebabize.ts
+++ src/utils/kebabize.ts
@@ -8,7 +8,7 @@ const kebabize = (str: string): string => {
     return ''
   }
 
-  return str.trim().replace(/ /g, '-').replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase()).replace(/--/g, '-')
+  return str.trim().replace(/ /g, '-').replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase()).replace(/--+/g, '-').replace(/-+$/g, '')
 }
 
 export default kebabize
