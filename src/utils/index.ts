5863ed792bc56812258415f3c0b989ec1582798f
Adam DeHaven <adam.dehaven@konghq.com>
2022-02-25 20:08:06 -0500

    chore(comment): add import comment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+// Do not use '@' alias in paths here so that imports within a consuming project resolve properly.
 export { default as registerCustomElement } from './registerCustomElement'
 export { default as kebabize } from './kebabize'
 export { default as helpText } from './helpText'
