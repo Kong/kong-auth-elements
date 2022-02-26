606c5d7a65c1134c5185707c2001f1a9c4a48095
Adam DeHaven <adam.dehaven@konghq.com>
2022-02-26 14:15:49 -0500

    chore(comment): fix comment

--- src/components/BaseCustomElement.vue
+++ src/components/BaseCustomElement.vue
@@ -14,7 +14,7 @@ export default defineComponent({
   name: 'BaseCustomElement',
 
   setup() {
-    // Must be present to inject child component styles for elements rendered inside of the KAuthWrapper <slot/>
+    // Must be present to inject child component styles for elements rendered inside of the <slot/>
     const { injectedStyles } = useInjectStyles()
 
     return {
