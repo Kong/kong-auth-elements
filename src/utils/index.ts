e4d395aa16cefdc06aca580c4dddacf51a7d82aa
Adam DeHaven <adam.dehaven@konghq.com>
2022-03-04 21:56:03 -0500

    test: add tests for custom error handler

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,29 @@
 // Do not use '@' alias in paths here so that imports within a consuming project resolve properly.
+import { AxiosError } from 'axios'
 export { default as registerCustomElement } from './registerCustomElement'
 export { default as kebabize } from './kebabize'
 export { default as helpText } from './helpText'
 export { default as win } from './window'
 
+// List of user entities
 export type UserEntities = 'user' | 'developer'
 
 // List of custom elements that accept a custom error handler
-export type CustomEndpointElements = 'kong-auth-forgot-password' | 'kong-auth-register'
+export type CustomEndpointElement = 'kong-auth-forgot-password' | 'kong-auth-register'
+
+// List of requests that support custom endpoints
+export type CustomEndpointRequest = 'reset-password-request' | 'register-request'
+
+export type CustomEndpointErrorEvent = {
+  error: AxiosError
+  request: CustomEndpointRequest
+  element: CustomEndpointElement
+}
 
 export interface KongAuthElementsOptions {
   apiBaseUrl?: string
   userEntity?: UserEntities
-  customErrorHandler?: (element: CustomEndpointElements, error: any) => string
+  customErrorHandler?: (event: CustomEndpointErrorEvent) => string
   shadowDom?: boolean
   shadowDomCss?: string[]
 }
