5c03f2f5061982b905e6c63370674eb167d8280b
Adam DeHaven <adam.dehaven@konghq.com>
2022-04-16 08:18:42 -0400

    fix: remove unused password meter code

--- src/components/PasswordStrengthMeter.vue
+++ src/components/PasswordStrengthMeter.vue
@@ -32,43 +32,6 @@ export default defineComponent({
       default: 7,
     },
     /**
-       * Display badge:
-       * The badge shows your
-       * password character count
-       * up to the defined secureLength
-       * @type {Boolean}
-       */
-    badge: {
-      type: Boolean,
-      default: true,
-    },
-    /**
-       * Show password toggle:
-       * Show icon to toggle
-       * the password visibility
-       */
-    toggle: {
-      type: Boolean,
-      default: false,
-    },
-    /**
-       * Prop to toggle the
-       * cleartext password if
-       * toggle is disabled
-       */
-    showPassword: {
-      type: Boolean,
-      default: false,
-    },
-    /**
-      * Prop to change the
-      * ref of the input
-      */
-    referenceValue: {
-      type: String,
-      default: 'input',
-    },
-    /**
        * Prop to toggle the
        * strength Meter if
        * User wants to implement
@@ -77,53 +40,6 @@ export default defineComponent({
     showStrengthMeter: {
       type: Boolean,
       default: true,
-    },
-    /**
-       * Prop to toggle the
-       * input element if
-       * User wants to implement
-       * their own input element
-       */
-    strengthMeterOnly: {
-      type: Boolean,
-      default: false,
-    },
-    /**
-       * CSS Class for the Input field
-       * @type {String}
-       */
-    defaultClass: {
-      type: String,
-      default: 'Password__field',
-    },
-    /**
-       * CSS Class for the disabled Input field
-       * @type {String}
-       */
-    disabledClass: {
-      type: String,
-      default: 'Password__field--disabled',
-    },
-    /**
-       * CSS Class for the badge
-       * if a password does not match
-       * the secureLength. Later for errors
-       * @type {String}
-       */
-    errorClass: {
-      type: String,
-      default: 'Password__badge--error',
-    },
-    /**
-       * CSS Class for the badge
-       * if a password does match
-       * the secureLength. Later for
-       * success messages possible.
-       * @type {String}
-       */
-    successClass: {
-      type: String,
-      default: 'Password__badge--success',
     },
     /**
        * CSS class for styling the
@@ -145,20 +61,6 @@ export default defineComponent({
       default: 'Password__strength-meter--fill',
     },
     /**
-       * Label for the show password icon
-       */
-    labelShow: {
-      type: String,
-      default: 'Show Password',
-    },
-    /**
-       * Label for the hide password icon
-       */
-    labelHide: {
-      type: String,
-      default: 'Hide Password',
-    },
-    /**
        * @type String
        */
     userInputs: {
@@ -174,15 +76,6 @@ export default defineComponent({
   },
 
   methods: {
-    togglePassword() {
-      if (this.shouldShowPassword) {
-        this.$emit('hide')
-        this.shouldShowPassword = false
-      } else {
-        this.$emit('show')
-        this.shouldShowPassword = true
-      }
-    },
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     emitValue(type, value, convertEvent = false) {
@@ -199,9 +92,9 @@ export default defineComponent({
 
   computed: {
     /**
-       * passwordStrength is the score calculated by zxcvbn
-       * @return {Number} Password Strength Score
-       */
+     * passwordStrength is the score calculated by zxcvbn
+     * @return {Number} Password Strength Score
+     */
     passwordStrength() {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
@@ -209,10 +102,10 @@ export default defineComponent({
     },
 
     /**
-       * isSecure checks if the length of the password is longer then
-       * the defined `secureLength`
-       * @return {Boolean} Password length longer then minLength
-       */
+     * isSecure checks if the length of the password is longer then
+     * the defined `secureLength`
+     * @return {Boolean} Password length longer then minLength
+     */
     isSecure() {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
@@ -220,44 +113,20 @@ export default defineComponent({
     },
 
     /**
-       * isActive checks if a password is entered.
-       * It's required for the password count badge.
-       * @return {Boolean} Password entered
-       */
-    isActive() {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      return this.password && this.password.length > 0
-    },
-
-    /**
-       * passwordCount holds the character count of the
-       * current password. It shows the count up to the `secureLength`.
-       * @return {Number} Password Character Count
-       */
+     * passwordCount holds the character count of the
+     * current password. It shows the count up to the `secureLength`.
+     * @return {Number} Password Character Count
+     */
     passwordCount() {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       return this.password && (this.password.length > this.secureLength ? `${this.secureLength}+` : this.password.length)
     },
-    /**
-       * Changing the input type from password to text
-       * based on the local shouldShowPassword data or the showPassword prop
-       */
-    inputType() {
-      return this.shouldShowPassword || this.showPassword ? 'text' : 'password'
-    },
-
-    showPasswordLabel() {
-      return this.shouldShowPassword || this.showPassword ? this.labelHide : this.labelShow
-    },
   },
 
   watch: {
     modelValue(newValue) {
-      if (this.strengthMeterOnly) {
-        this.emitValue('input', newValue)
-      }
+      this.emitValue('input', newValue)
       this.$emit('feedback', zxcvbn(newValue).feedback)
     },
     passwordStrength(score) {
