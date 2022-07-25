a1419dafa30860676029968d9df9bf0615930eb2
Adam DeHaven <adam.dehaven@konghq.com>
2022-07-25 10:36:39 -0400

    feat(vite): upgrade to vite v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig, loadEnv } from 'vite'
+import dns from 'dns'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
+
+// You can set dns.setDefaultResultOrder('verbatim') to disable the reordering behavior. Vite will then print the address as localhost
+// https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder('verbatim')
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
