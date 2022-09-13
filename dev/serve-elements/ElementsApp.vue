27a9cd5151df3c7be921527edc03c232a618afcb
Adam DeHaven <adam.dehaven@konghq.com>
2022-09-12 20:46:02 -0400

    feat(recaptcha): add reCAPTCHA to the register form [KHCP-4759]

--- dev/serve-elements/ElementsApp.vue
+++ dev/serve-elements/ElementsApp.vue
@@ -59,6 +59,7 @@
         <div id="kong-auth-register-wrapper">
           <kong-auth-register
             access-code-required
+            recaptcha
             wrapper-id="kong-auth-register-wrapper"
             @register-success="showAlert('Register success!')" />
         </div>
