414a725e070d45e61d843a3d1ffccbfe5c90b448
portikM <maksym.portianoi@konghq.com>
2023-01-27 17:03:59 -0500

    chore(stylelint): add stylelint config

---
+++ .stylelintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'stylelint-config-html',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-recommended-vue/scss'
+  ],
+  overrides: [
+    {
+      files: [
+        '**/*.vue',
+        '**/*.scss'
+      ],
+      rules: {
+        'unit-disallowed-list': [
+          ['rem', 'em'],
+        ],
+        // Disable the following rules
+        'custom-property-no-missing-var-function': null,
+        'no-descending-specificity': null,
+      }
+    }
+  ],
+  plugins: [ 'stylelint-order' ],
+  rules: { 'order/properties-alphabetical-order': true }
+}
