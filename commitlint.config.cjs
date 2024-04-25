dd051ad8a819d2f030cfe04efaedba56a4abf18d
Adam DeHaven <adam.dehaven@konghq.com>
2024-04-25 16:39:37 -0400

    chore: commitlint

--- commitlint.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 108],
-  },
-  ignores: [(message) => /^chore\(release\): .+$/m.test(message)],
-}
